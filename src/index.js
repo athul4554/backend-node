6af4078f55711fcf5d15cd73116981f646dd6d24
athul4554 <athul.babu@bridgeweave.com>
2024-07-19 23:40:23 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./env" });
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running in port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("MongoDB COonn failed!!!", err));
+
+/*
+import express from "express";
+
+const app = express()
+
+;(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Err: ",error)
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("Err; ",error)
+        throw error
+    }
+})()
+*/
